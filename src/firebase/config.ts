7df25690063f3f998f1d9634710234596508ae7c
Cash Center <cashcenter.fun@gmail.com>
2025-11-09 15:00:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3669304322-ab97b",
+  "appId": "1:530415008831:web:94354ad6011413216e4c8a",
+  "apiKey": "AIzaSyDH_gKfm1JMSAJd0GlrFWX7-moAckNiOAw",
+  "authDomain": "studio-3669304322-ab97b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "530415008831"
+};

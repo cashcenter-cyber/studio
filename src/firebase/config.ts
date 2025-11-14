e7a4668d06ebc758a08e4e79afa2dc43408c5218
Cash Center <cashcenter.fun@gmail.com>
2025-11-14 11:02:37 +0000

    AIzaSyDH_gKfm1JMSAJd0GlrFWX7-moAckNiOAw
    tien l'api

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 // This configuration is public and safe to expose to the client.
 // Security is enforced by Firebase Security Rules on the backend.
 export const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDH_gKfm1JMSAJd0GlrFWX7-moAckNiOAw",
+    authDomain: "cash-center-fun.firebaseapp.com",
+    projectId: "cash-center-fun",
+    storageBucket: "cash-center-fun.appspot.com",
+    messagingSenderId: "31868832103",
+    appId: "1:31868832103:web:96452a8b309a65c2b0b14a",
+    measurementId: "G-9XG991J5P9"
 };
